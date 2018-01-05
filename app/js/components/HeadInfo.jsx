794bd3c242997e521bbe1eb97d95bdb6de49fa6e
Justin Brazeau <justinb@live.ca>
2018-01-05 21:33:56 +0700

    fix meta tag responsive

--- app/js/components/HeadInfo.jsx
+++ app/js/components/HeadInfo.jsx
@@ -7,6 +7,7 @@ class HeadInfo extends React.Component {
     return (
       <Helmet>
         <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
